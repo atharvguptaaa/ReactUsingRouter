abc69e7cc9ae1b99b2ff6c0e855b0d26aca62a8b
atharvguptaaa <workinatharv@gmail.com>
2024-03-07 01:36:08 +0530

    c

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -73,7 +73,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                            atharvguptaaa
                         </a>
                         . All Rights Reserved.
                     </span>
